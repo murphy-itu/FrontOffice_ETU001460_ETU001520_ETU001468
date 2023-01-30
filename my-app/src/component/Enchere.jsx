145bf16a1d5c0b23194f268ec83ae462200e51f1
Murphy <murphytsirofozafy@gmail.com>
2023-01-30 20:43:23 +0300

    liste-enchere-component

---
+++ my-app/src/component/Enchere.jsx
@@ -0,0 +1,32 @@
+import { Component } from "react";
+import '../assets/css/Enchere.css';
+
+class Enchere extends Component {
+    enchere = this.props.enchere;  
+    render() {
+        return (
+            <div class="container" >
+                <div class="item item-cont" >
+                    {
+                        this.enchere.photo.map((photo,index) =>
+                            <div class="item-it"><img key={photo.id} src={"data:image/jpeg;base64,"+photo.base64} /></div>
+                            
+                        )
+                    }
+                </div>
+                <div class="item" >
+                    <div>
+                        <h1>{this.enchere.produit.nom}</h1>
+                        <p>categorie : <strong>{this.enchere.produit.categorie.nom}</strong> </p>
+                        <p>prix minimal : <strong>{this.enchere.prixminimal}</strong> </p>  
+                        <p> date : <strong> {this.enchere.datetime} </strong> </p>
+                        <p>{this.enchere.description}</p>
+                        <p> <a href=""><button>rencherir</button></a></p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Enchere;
---
+++ my-app/src/index.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+// import HelloWord from './pages/Grid';
+import Enchere from './component/Enchere'
+
+const enchere = [
+  {
+  utilisateur:{
+    id:1,
+    nom:"RAKOTO",
+    prenom:"Jean",
+    email:"jean@gmail.com",
+    motdepasse:"12345"  
+  },
+  produit:{
+    id:1,
+    nom:"Produit 1",
+    categorie:{
+      id:1,
+      nom:"Categorie 1"
+    }
+  },
+  description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  prixminimal:130000,
+  datetime:"2022-01-01 15:00:00",
+  durree:"05:00:00",
+  etat:1,
+  photo:[
+    {
+      id:1,
+      ienchere:1,
+      base64:"fopajafvtrhhrtbhhhhhhhhhhhhhhhhhhhhh"
+    },
+    {
+      id:2,
+      ienchere:1,
+      base64:"kalkaopkpockpaokofpkapok"
+    },
+    {
+      id:3,
+      ienchere:1,
+      base64:"kalkaopkpockpaokofpkapok"
+    },{
+      id:3,
+      ienchere:1,
+      base64:"kalkaopkpockpaokofpkapok"
+    }
+  ] ,
+
+} , 
+{
+    utilisateur:{
+      id:2,
+      nom:"RASOA",
+      prenom:"Jeanne",
+      email:"jeannz@gmail.com",
+      motdepasse:"12345"  
+    },
+    produit:{
+      id:1,
+      nom:"Produit 2",
+      categorie:{
+        id:1,
+        nom:"Categorie 2"
+      }
+    },
+    description:" (2) Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    prixminimal:300000,
+    datetime:"2023-01-01 15:00:00",
+    durree:"01:00:00",
+    etat:1,
+    photo:[
+      {
+        id:1,
+        ienchere:1,
+        base64:"fopajafvtrhhrtbhhhhhhhhhhhhhhhhhhhhh"
+      },
+      {
+        id:2,
+        ienchere:1,
+        base64:"kalkaopkpockpaokofpkapok"
+      }
+    ] ,
+}
+]
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// root.render(
+//   <React.StrictMode>
+//     <App/>
+//   </React.StrictMode>
+// );
+
+root.render (
+    enchere.map((e,index) => {
+      return <Enchere key={index} enchere={e} />
+    })
+);
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
