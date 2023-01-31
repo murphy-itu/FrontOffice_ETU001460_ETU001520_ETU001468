ed65c7cf8594c277b4edd46dd56c8c4eab37b191
Murphy <murphytsirofozafy@gmail.com>
2023-01-31 09:59:30 +0300

    update_add_photo_liste

--- my-app/src/component/Enchere.jsx
+++ my-app/src/component/Enchere.jsx
@@ -6,22 +6,21 @@ class Enchere extends Component {
     render() {
         return (
             <div class="container" >
-                {/* <div class="item item-cont" >
+                <div class="item item-cont" >
                     {
-                        this.enchere.photo.map((photo,index) =>
-                            <div class="item-it"><img key={photo.id} src={"data:image/jpeg;base64,"+photo.base64} /></div>
-                            
+                        this.enchere.photos.map((photo,index) =>
+                            <div class="item-it"><img key={index} src={"data:image/jpeg;base64,"+photo.base64} /></div> 
                         )
                     }
-                </div> */}
+                </div>
                 <div class="item" >
                     <div>
                         <h1>{this.enchere.produit.nom}</h1>
                         <p>categorie : <strong>{this.enchere.produit.categorie.nom}</strong> </p>
                         <p>prix minimal : <strong>{this.enchere.prixminimal}</strong> </p>  
-                        <p> date : <strong> {this.enchere.datetime} </strong> </p>
+                        <p>date : <strong> {this.enchere.datetime} </strong> </p>
                         <p>{this.enchere.description}</p>
-                        <p> <a href=""><button>rencherir</button></a></p>
+                        <p><a href=""><button>rencherir</button></a></p>
                     </div>
                 </div>
             </div>
