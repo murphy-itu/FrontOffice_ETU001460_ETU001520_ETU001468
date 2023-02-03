7335b2d4ac762ecdbc54b6853fe174c1491617b1
ValiahRakotozanany <99878610+ValiahRakotozanany@users.noreply.github.com>
2023-02-03 11:41:57 +0300

    Image

--- my-app/my-appnpx/src/component/Enchere.jsx
+++ my-app/my-appnpx/src/component/Enchere.jsx
@@ -22,13 +22,14 @@ class Enchere extends Component {
         return (
               
             <div class="container" >
+                 <div class="item">
                 <div class="item-it" >
                     {
                         this.enchere.photos.map((photo,index) =>
-                            <div class="item-it"><img class="img-fluid" key={index} src={""+photo.base64} /></div> 
+                           <img class="img-fluid" key={index} src={""+photo.base64} />
                         )
                     }
-                </div>                
+                </div>       </div>                
                 <div class="item" >
                     <div>
                         <h1>{this.enchere.produit.nom}</h1>
