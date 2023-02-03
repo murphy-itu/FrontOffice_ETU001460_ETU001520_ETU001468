567089ae47fd11f7f3ecee506966f5671e586988
Murphy <murphytsirofozafy@gmail.com>
2023-02-03 04:27:24 +0300

    fiche

---
+++ my-app/my-appnpx/src/component/Fiche.jsx
@@ -0,0 +1,31 @@
+import { Component } from "react";
+import { useHistory } from "react-router-dom";
+class Fiche extends Component {
+    ficheenchere = this.props.ficheenchere;  
+    render() {
+        return (
+            <div>
+                <table>
+                    <tr>
+                        <th>Nom</th>
+                        <th>Prenom</th>
+                        <th>montant</th>
+                        <th>Status</th>
+                    </tr>
+                    <tr>
+                        <td>{this.ficheenchere.utilisateur.nom}</td>
+                        <td>{this.ficheenchere.utilisateur.Prenom}</td>
+                        <td>{this.ficheenchere.montant}</td>
+                        <td>
+                            {
+                                (this.ficheenchere.status == 1)?("en-tête"):("depassé")
+                            }
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        )
+    }
+}
+
+export default Fiche;
